728923f700339de27e7ab5dbb5cfdd64eb706c03
André Carvalho <afacarvalho@yahoo.com.br>
2020-11-05 19:15:02 -0300

    Fixing paths using environment variables

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # The view date reference of DETER used to deter_rasterize in the SQL filter. (view_date >= '2019-08-01')
 DETER_VIEW_DATE="2019-08-01"
-# The script work directory.
-DATA_DIR=`pwd`
-DATA_DIR=$DATA_DIR"/../data"
+# The data work directory.
+DATA_DIR=$SHARED_DIR
 export DATA_DIR
+# go to the scripts directory
+cd $SCRIPT_DIR
 # get focuses and alerts for last month
 python3 download-month-data.py
 
