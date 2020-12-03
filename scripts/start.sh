dc2b926684b298d34b28c9ae3ebe6e007c8dfcf3
André Carvalho <afacarvalho@yahoo.com.br>
2020-12-03 10:28:21 -0300

    Disable debug lines

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # to debug in localhost
-SCRIPT_DIR=`pwd`
-SHARED_DIR=$SCRIPT_DIR"/../data"
+# SCRIPT_DIR=`pwd`
+# SHARED_DIR=$SCRIPT_DIR"/../data"
 # to store run log
 DATE_LOG=$(date +"%Y-%m-%d_%H:%M:%S")
 # The data work directory.
