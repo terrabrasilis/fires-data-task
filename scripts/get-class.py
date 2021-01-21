1a12d78595bc5c75b107e543f9eb5c0161b81007
André Carvalho <afacarvalho@yahoo.com.br>
2021-01-21 14:27:27 -0300

    Set the default value for any focus that is NULL.

--- scripts/get-class.py
+++ scripts/get-class.py
@@ -113,6 +113,14 @@ def run(host='localhost', port='5432', database='dbname', user='postgres', passw
     print (query,id,lat,lon,pixelvalue,pixelclasse)
       
   con.commit()
+
+  # Finally, if you have NULL values for any focus, set the default value.
+  query = "UPDATE public.focos_aqua_referencia SET classe_prodes='Outros' WHERE classe_prodes IS NULL;"
+	cur.execute(query)
+  query = "UPDATE public.focos_aqua_referencia SET classe_car='Sem CAR' WHERE classe_car IS NULL;"
+  cur.execute(query)
+  con.commit()
+
   cur.close()
   con.close()
 
