69ae488046f1e717065c7698748137304ad0e799
André Carvalho <afacarvalho@yahoo.com.br>
2021-09-08 17:45:23 -0300

    Include time zone definition, TZ variable, in environment file

--- env-scripts/docker-entrypoint.sh
+++ env-scripts/docker-entrypoint.sh
@@ -3,5 +3,6 @@
 ## SO, WE WRITE INSIDE THE /etc/environment FILE AND READS BEFORE RUN THE SCRIPT.
 echo "export SHARED_DIR=\"$SHARED_DIR\"" >> /etc/environment
 echo "export SCRIPT_DIR=\"$SCRIPT_DIR\"" >> /etc/environment
+echo "export TZ=\"America/Sao_Paulo\"" >> /etc/environment
 # run cron in foreground
 cron -f
