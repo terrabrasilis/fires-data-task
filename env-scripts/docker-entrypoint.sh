d5317986aaddf64c42e826c086fb1031cff23a50
andre-carvalho <afacarvalho@yahoo.com.br>
2024-10-10 08:50:58 -0300

    Remove environment variables

--- env-scripts/docker-entrypoint.sh
+++ env-scripts/docker-entrypoint.sh
@@ -5,10 +5,7 @@ echo "export SHARED_DIR=\"$SHARED_DIR\"" >> /etc/task_environment
 echo "export DOWNLOAD_AREA=\"$DOWNLOAD_AREA\"" >> /etc/task_environment
 echo "export SCRIPT_DIR=\"$SCRIPT_DIR\"" >> /etc/task_environment
 echo "export TZ=\"America/Sao_Paulo\"" >> /etc/task_environment
-echo "export PG_DETER_HOST=\"$PG_DETER_HOST\"" >> /etc/task_environment
-echo "export PG_DETER_PORT=\"$PG_DETER_PORT\"" >> /etc/task_environment
-echo "export PG_FIRES_HOST=\"$PG_FIRES_HOST\"" >> /etc/task_environment
-echo "export PG_FIRES_PORT=\"$PG_FIRES_PORT\"" >> /etc/task_environment
+# Its optional, but if not defined, the default is the last prodes year.
 echo "export DETER_VIEW_DATE=\"$DETER_VIEW_DATE\"" >> /etc/task_environment
 # run cron in foreground
 cron -f
