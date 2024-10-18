4ebd2e9ceeb8f059e10b67bdd5853e28013859fc
andre-carvalho <afacarvalho@yahoo.com.br>
2024-10-18 17:15:00 -0300

    Fix the missing library into Dockerfile

--- env-scripts/exec_monthly.sh
+++ env-scripts/exec_monthly.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /etc/task_environment
-"$SCRIPT_DIR/start.sh"
+DATE_LOG=$(date +"%Y-%m-%d_%H:%M:%S")
+${SCRIPT_DIR}"/start.sh" >> "/usr/local/data/fires_data_task_"${DATE_LOG}".log" 2>&1
